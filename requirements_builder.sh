7916c5e6eeef0233b947def91680032383ff1241
fre171@csiro.au <andrew.freebairn@csiro.au>
2025-07-18 16:15:42 +1000

    Clean requirements

--- requirements_builder.sh
+++ requirements_builder.sh
@@ -1,2 +1,3 @@
-poetry export -f requirements.txt --output requirements.txt --without-hashes
+poetry export -f requirements.txt --output dirty_requirements.txt --without-hashes
 
+awk -F';' '{print $1}' dirty_requirements.txt > requirements.txt
