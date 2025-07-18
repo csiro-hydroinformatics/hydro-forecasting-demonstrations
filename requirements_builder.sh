25f271c7bcbae612a202effb3b4630a058b0ee72
fre171@csiro.au <andrew.freebairn@csiro.au>
2025-07-18 16:26:19 +1000

    Remove version numbers, might work?

--- requirements_builder.sh
+++ requirements_builder.sh
@@ -1,3 +1,3 @@
 poetry export -f requirements.txt --output dirty_requirements.txt --without-hashes
 
-awk -F';' '{print $1}' dirty_requirements.txt > requirements.txt
+awk -F'=' '{print $1}' dirty_requirements.txt > requirements.txt
